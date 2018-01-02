a815a9f2879ee321dd0169e36ad4f7ff5e0a86e2
中本昌吾 <nakamotoshogo@nakamotoshougonoMacBook-Pro.local>
2018-01-03 00:06:47 +0900

    add constructers

--- app/Http/Controllers/StateController.php
+++ app/Http/Controllers/StateController.php
@@ -10,10 +10,16 @@ use Illuminate\Support\Collection;
 
 class StateController extends Controller
 {
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
     public function __construct()
     {
         $this->middleware('auth');
     }
+
     /**
      * Display a listing of the resource.
      *
